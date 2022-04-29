3ceabab481d653c6df519c72ffbd6035d1a737de
Rohit Agarwal <programmingwithrohit@gmail.com>
2022-04-30 00:13:03 +0530

    Added new exception

---
+++ 3.spring-reactive-app/src/main/java/com/github/javamultiplex/error/ProductRetrieveException.java
@@ -0,0 +1,8 @@
+package com.github.javamultiplex.error;
+
+/**
+ * @author Rohit Agarwal on 30/04/22 12:05 AM
+ * @copyright github.com/javamultiplex
+ */
+public class ProductRetrieveException extends RuntimeException{
+}
--- 3.spring-reactive-app/src/main/java/com/github/javamultiplex/proxy/ProductProxy.java
+++ 3.spring-reactive-app/src/main/java/com/github/javamultiplex/proxy/ProductProxy.java
@@ -12,10 +12,29 @@ import reactor.core.publisher.Flux;
 @Component
 public record ProductProxy(WebClient webClient) {
 
-    public Flux<Product> getAllProducts(){
+    public Flux<Product> getAllProducts() {
         return webClient
                 .get()
                 .uri("/products")
-                .exchangeToFlux(res -> res.bodyToFlux(Product.class));
+                .exchangeToFlux(res -> res.bodyToFlux(Product.class))
+                //.onErrorResume(e -> Flux.just(new Product("dummy")));
+                //.onErrorResume(WebClientRequestException.class, e -> Flux.just(new Product("dummy")));
+                //.onErrorResume(e -> e.getMessage()==null, e-> Flux.just(new Product("dummy")));
+                //.onErrorReturn(new Product("dummy"));
+                //.onErrorReturn(WebClientRequestException.class, new Product("dummy"));
+                //.onErrorReturn(e -> e.getMessage() == null, new Product("dummy"));
+                //.onErrorMap(e -> new ProductRetrieveException());
+                //.onErrorMap(WebClientRequestException.class, e-> new ProductRetrieveException());
+                //.onErrorMap(e->e.getMessage()==null,e -> new ProductRetrieveException());
+//                .doOnNext(p -> {
+//                    if (p.name() == null) throw new RuntimeException();
+//                })
+//                .onErrorContinue((e, o) -> System.out.println(e.getMessage()));
+                //.onErrorContinue(WebClientRequestException.class, (e,o)-> System.out.println(e.getMessage()))
+                //.onErrorContinue(e -> e.getMessage() == null, (e, o) -> System.out.println(e.getMessage()));
+                //.retry();
+                .retry(3);
+
+
     }
 }
